ca81872875530bc2f7394cb6cd6e49746c532749
Mitch <mitchel.langford@southwales.ac.uk>
2016-09-20 13:05:04 +0100

    commit3
    have added image into resources
    have global variables atX and atY to track sprite location
    have a function to find the label at position (x,y)
    have functions to display/undisplay the sprite at location (x,y)
    have initialised the sprite to show at its start position

--- minefield/Form1.cs
+++ minefield/Form1.cs
@@ -15,6 +15,47 @@ namespace minefield
         public Form1()
         {
             InitializeComponent();
+
+            //place the sprite at its start-up location
+            drawspite(atX, atY);
         }
+
+        int atX = 10;
+        int atY = 20;
+
+        //function to return the Label at location (x,y)
+        private Label getLabel(int x, int y)
+        {
+            int k = (y - 1) * 20 + x;
+            string s = "label" + k.ToString();
+
+            foreach (Control c in panel1.Controls)
+            {
+                if (c.GetType() == typeof(System.Windows.Forms.Label))
+                {
+                    if (c.Name == s)
+                    {
+                        return (Label)c;
+                    }
+                }
+            }
+            return null;
+        }
+
+        //function to draw the sprite at location (x,y)
+        private void drawspite(int x, int y)
+        {
+            Label lbl = getLabel(atX, atY);
+            lbl.BackColor = Color.White;
+            lbl.Image = Properties.Resources.w;
+        }
+
+        //function to undraw the sprite at location (x,y)
+        private void wipespite(int x, int y)
+        {
+            Label lbl = getLabel(atX, atY);
+            lbl.Image = null;
+        }
+
     }
 }
